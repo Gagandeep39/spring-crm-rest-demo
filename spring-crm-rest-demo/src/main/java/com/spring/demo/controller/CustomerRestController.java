8de3a90c650f93d8c75340094f8512f1dcd6e7c6
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-04-11 01:14:24 +0530

    Added CustomerController Added fetch all api

---
+++ spring-crm-rest-demo/src/main/java/com/spring/demo/controller/CustomerRestController.java
@@ -0,0 +1,29 @@
+/**
+ * Gagandeep
+ * 1:11:51 am
+ * 11-Apr-2020
+ */
+package com.spring.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.spring.demo.entity.Customer;
+import com.spring.demo.service.CustomerService;
+
+@RestController
+@RequestMapping("/api")
+public class CustomerRestController {
+	
+	@Autowired
+	private CustomerService service;
+
+	@GetMapping("/customers")
+	public List<Customer> getCustomers(){
+		return service.getCustomers();
+	}
+}
