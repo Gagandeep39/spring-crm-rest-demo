64bc3fc980c17e3088ec9d74d014f3f321f8c81f
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-04-11 11:31:31 +0530

    Fixed getbyId method Missing @PathVariable

--- spring-crm-rest-demo/src/main/java/com/spring/demo/controller/CustomerRestController.java
+++ spring-crm-rest-demo/src/main/java/com/spring/demo/controller/CustomerRestController.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -38,7 +39,7 @@ public class CustomerRestController {
 	 * @return Customer
 	 */
 	@GetMapping("/customers/{id}")
-	public Customer getCustomerById(int id) {
+	public Customer getCustomerById(@PathVariable int id) {
 		return service.getCustomer(id);
 	}
 }
