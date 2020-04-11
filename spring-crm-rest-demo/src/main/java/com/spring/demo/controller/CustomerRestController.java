88973e4d0df5b64f68e25a145207e4b90b63544d
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-04-11 11:57:05 +0530

    Added a post method to add customer

--- spring-crm-rest-demo/src/main/java/com/spring/demo/controller/CustomerRestController.java
+++ spring-crm-rest-demo/src/main/java/com/spring/demo/controller/CustomerRestController.java
@@ -10,6 +10,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -46,6 +48,23 @@ public class CustomerRestController {
 		Customer customer = service.getCustomer(id);
 		if(customer == null) 
 			throw new CustomerNotFoundException("Customer not found with ID " + id);
+		return customer;
+	}
+	
+	/**
+	 * Request URL: http://localhost:8083/spring-crm-rest/api/customers
+	 * {
+	 * 	"firstName": "Gagan",
+	 *  "lastName" : "Singh",
+	 *  "email" : test@mail.com
+	 * }
+	 * Post method to save custoer
+	 * @return Customer
+	 */
+	@PostMapping("/customers")
+	public Customer addCustomer(@RequestBody Customer customer) {
+		service.saveCustomer(customer);
+		// Here the customer will have an Updated ID
 		return customer;
 	}
 }
